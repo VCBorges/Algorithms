2d3ef1ed9f22e6a62bb6b7156c93332f2046949a
VCBorges <vinicius.costa.borges98@gmail.com>
2024-05-31 21:35:32 -0300

    added iterative binary search

--- src/searching/binary_search.py
+++ src/searching/binary_search.py
@@ -15,6 +15,24 @@ def recursive_binary_search(
     
     return recursive_binary_search(arr[:midpoint], key, start)
 
+
+def iterative_binary_search(
+    arr: list[int], 
+    key: int,
+) -> int | None:
+    high = len(arr) - 1
+    low = 0
+    while low <= high:
+        midpoint = low + ((high - low) // 2)
+        if key == arr[midpoint]:
+            return midpoint
+        elif key < arr[midpoint]:
+            high = midpoint - 1
+        else:
+            low = midpoint + 1
+    return None
+
 input = [1,2,3,4,5,6,7,8,9,10]
 
-print(recursive_binary_search(input, 10))
+
+print(iterative_binary_search(input, 1))
