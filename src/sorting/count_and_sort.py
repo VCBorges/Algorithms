8fb81d49ca492a351322127067b8a34adf3e5489
VCBorges <vinicius.costa.borges98@gmail.com>
2024-06-09 16:39:18 -0300

    count and sort

---
+++ src/sorting/count_and_sort.py
@@ -0,0 +1,25 @@
+from rich import print
+
+def count_and_sort(arr: list[int]) -> list[int]:
+    if not arr:
+        return arr
+    
+    M = max(arr) + 1
+    counter = [0] * M
+
+    for i in arr:
+        counter[i] += 1
+    
+    position = [0] * M
+    for i in range(2, M):
+        position[i] = position[i - 1] + counter[i - 1]
+        
+    sorted_arr = [0] * len(arr)
+    for i in range(1, len(arr)):
+        sorted_arr[position[arr[i]]] = arr[i]
+        position[arr[i]] += 1
+    
+    return sorted_arr
+
+
+input = [2,2,2,1,1,1,3,3]
